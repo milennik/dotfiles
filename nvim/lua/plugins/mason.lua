8cae1af1a6f15a5077ac7741863df858ed03de07
Nikola <nikola.milenkovic@outlook.com>
2022-08-06 21:41:07 +0200

    feat(nvim): Add nvim config files

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,10 @@
+-----------------------------------------------------------
+-- Mason LSP installer configuration file
+-----------------------------------------------------------
+
+-- Plugin: williamboman/mason.nvim
+-- url: https://github.com/williamboman/mason.nvim#requirements
+
+
+
+require("mason").setup()
