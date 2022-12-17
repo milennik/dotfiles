5300c4ba7b06e61db05aab2b6ff02bb008f609c2
Nikola <nikola.milenkovic@outlook.com>
2022-12-17 23:45:30 +0100

    Update nvim kickstart config.

--- nvim_kickstart/init.lua
+++ nvim_kickstart/init.lua
@@ -68,6 +68,8 @@ require('packer').startup(function(use)
     end
   }
 
+  -- Git diff
+  use { "milennik/gitdiff.nvim" }
 
   -- Add custom plugins to packer from /nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
@@ -104,16 +106,7 @@ vim.api.nvim_create_autocmd('BufWritePost', {
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
-vim.opt.colorcolumn = "80"
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.swapfile = false
-vim.opt.termguicolors = true
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -144,6 +137,44 @@ vim.cmd [[colorscheme onedark]]
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
+
+local g = vim.g       -- Global variables
+local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+
+opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
+-----------------------------------------------------------
+-- Neovim UI
+-----------------------------------------------------------
+opt.number = true           -- Show line number
+opt.showmatch = true        -- Highlight matching parenthesis
+opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+opt.colorcolumn = '80'      -- Line lenght marker at 80 columns
+opt.splitright = true       -- Vertical split to the right
+opt.splitbelow = true       -- Horizontal split to the bottom
+opt.ignorecase = true       -- Ignore case letters when search
+opt.smartcase = true        -- Ignore lowercase for the whole pattern
+opt.linebreak = true        -- Wrap on word boundary
+opt.termguicolors = true    -- Enable 24-bit RGB colors
+opt.laststatus=3            -- Set global statusline
+opt.relativenumber = true
+-----------------------------------------------------------
+-- Tabs, indent
+-----------------------------------------------------------
+opt.expandtab = true        -- Use spaces instead of tabs
+opt.shiftwidth = 4          -- Shift 4 spaces when tab
+opt.tabstop = 4             -- 1 tab == 4 spaces
+opt.smartindent = true      -- Autoindent new lines
+opt.list = true
+opt.listchars:append "eol:↴"
+opt.listchars:append "space:⋅"
+-----------------------------------------------------------
+-- Memory, CPU
+-----------------------------------------------------------
+opt.hidden = true           -- Enable background buffers
+opt.history = 100           -- Remember N lines in history
+opt.lazyredraw = true       -- Faster scrolling
+opt.synmaxcol = 240         -- Max column for syntax highlight
+opt.updatetime = 700        -- ms to wait for trigger an event
 
 -- [[ Basic Keymaps ]]
 -- Set <space> as the leader key
@@ -354,6 +385,19 @@ local on_attach = function(_, bufnr)
   nmap('<leader>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, '[W]orkspace [L]ist Folders')
+
+
+-- Git diff
+nmap('n', '<C-y>', ":lua require('gitdiff').diff('master')<cr>")
+-- map('n', '<C-[>', ":lua require('gitdiff').diff()<cr>")
+nmap('n', '<C-p>', ":Gvdiffsplit master<cr>")
+-- Telescope
+nmap('gv', ":lua require('telescope.builtin').git_commits()<cr>")
+nmap('gy', ":lua require('telescope.builtin').git_branches()<cr>")
+nmap('gt', ":lua require('telescope.builtin').git_status()<cr>")
+nmap('gh', ":lua require('telescope.builtin').git_stash()<cr>")
+
+
 
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
